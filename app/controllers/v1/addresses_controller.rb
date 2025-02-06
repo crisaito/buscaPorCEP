b56d93a9e9720f0b1fa3290e73d69ed3f59293c2
hitomi <crhisaito@gmail.com>
2025-02-05 22:20:19 -0300

    feat: add seach controller error scenarios

--- app/controllers/v1/addresses_controller.rb
+++ app/controllers/v1/addresses_controller.rb
@@ -6,9 +6,17 @@ module V1
 
     def search
       uri = URI("#{BASE_URL}#{params[:cep]}")
-      address = Net::HTTP.get(uri)
+      response = Net::HTTP.get_response(uri)
+      address = JSON.parse(response.body)
 
-      render json: address, status: :ok
+      case response.code
+      when "200"
+        render json: address, status: :ok
+      when "400"
+        render json: { erro: address['message'] }, status: :bad_request
+      when "404"
+        render json: { erro: address['message'] }, status: :not_found
+      end
     end
   end
 end
