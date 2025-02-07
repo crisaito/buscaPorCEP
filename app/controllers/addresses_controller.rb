2b5e205d50c4b032c7d72101def132861aab26bd
hitomi <crhisaito@gmail.com>
2025-02-07 01:03:59 -0300

    feat: add layout to most searched ceps

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -4,6 +4,8 @@ class AddressesController < ApplicationController
   BASE_URL = "https://cep.awesomeapi.com.br/json/"
 
   def search
+    @most_searched_ceps = Address.most_searched
+
     uri = URI("#{BASE_URL}#{params[:cep]}")
     response = Net::HTTP.get_response(uri)
     @address = JSON.parse(response.body)
