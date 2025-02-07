6a8289ece9a8b6eb8726fb665c4de19f55492aa8
hitomi <crhisaito@gmail.com>
2025-02-07 01:16:44 -0300

    test: add shoulda_matchers and add model Address tests

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,28 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:cep) }
+    it { is_expected.to validate_presence_of(:city) }
+    it { is_expected.to validate_presence_of(:state) }
+
+    context 'CEP' do
+      it 'is valid when format is correct' do
+        address = Address.new(cep: '12345678', city: 'São Paulo', state: 'SP')
+        expect(address).to be_valid
+      end
+
+      it 'is invalid when less than 8 digits' do
+        address = Address.new(cep: '1234567', city: 'São Paulo', state: 'SP')
+        expect(address).not_to be_valid
+      end
+
+      it 'is invalid when more than 8 digits' do
+        address = Address.new(cep: '123456789', city: 'São Paulo', state: 'SP')
+        expect(address).not_to be_valid
+      end
+    end
+  end
 end
